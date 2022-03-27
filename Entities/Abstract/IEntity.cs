d8dcd5ca44d9ec0f944072780d31920f4cd5f1f5
Bilal Can Altındağ <bilalcanaltindag@hotmail.com>
2022-03-27 16:02:27 +0300

    DataAccess, Business and Entities added

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{   //Class that implements IEntity is the Database table
+    public interface IEntity
+    {
+    }
+}
